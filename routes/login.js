d186beca8c5c600e76ec24d4c2a4522c81241c61
ChrissiQ <rediahs@gmail.com>
2015-04-21 17:37:26 -0600

    Enable login routes

--- routes/login.js
+++ routes/login.js
@@ -10,24 +10,24 @@ router.get('/', function(req, res, next) {
   if (res.err) res.render('Error!' + res.err)
   // req.login(user, function(err) {
   //   if (err) { return next(err); }
-  //   return res.redirect('/users/' + req.user.username);
+  //   return res.redirect('/users/' + req.user.name);
   // });
   res.render('login', { title: 'Chrissi Klassen'})
 });
 
-// router.post('/',
-//   passport.authenticate('local', {
-//     successRedirect: '/',
-//     failureRedirect: '/login'
-//   })
-// );
+router.post('/',
+  passport.authenticate('local', {
+    successRedirect: '/login/success',
+    failureRedirect: '/login/failed'
+  })
+);
 
-// router.get('/failed', function(req, res, next) {
-//   res.send('Failed to authenticate');
-// });
+router.get('/failed', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
  
-// router.get('/success', function(req, res, next) {
-//   res.send('Successfully authenticated');
-// });
+router.get('/success', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
 
 export default router;
