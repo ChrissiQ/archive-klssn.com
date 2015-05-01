8953c02f4acda05c35ffefaaf27e9b76300c9028
ChrissiQ <rediahs@gmail.com>
2015-05-01 03:59:57 -0600

    Validate role name.

--- app/models/role.js
+++ app/models/role.js
@@ -1,7 +1,11 @@
 import Sequelize from 'sequelize';
 
 let RoleSchema = {
-  name: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  }
 };
 
 let options = {
