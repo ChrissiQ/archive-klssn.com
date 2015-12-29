c58cf5356578468798b4ba67eef6bf8a34a452d7
ChrissiQ <rediahs@gmail.com>
2015-12-29 01:01:01 -0700

    Fix problem with loading secrets for express-session.

--- app/services/express-session.js
+++ app/services/express-session.js
@@ -1,12 +1,14 @@
 import redis from 'connect-redis';
 import session from 'express-session';
-import secret from '../config/secret';
+import * as secret from '../config/secret';
 
+let env = process.env.NODE_ENV || "development";
+let config = secret[env];
 let RedisStore = redis(session);
 
 let redisSession = session({
-  store: new RedisStore(secret.redis),
-  secret: secret.session_secret,
+  store: new RedisStore(config.redis),
+  secret: config.session_secret,
   resave: true,
   saveUninitialized: true
 });
