805bb45645fe260e019101d115d124a2802ffa94
ChrissiQ <rediahs@gmail.com>
2015-12-29 22:18:30 -0700

    WIP - index style.

--- gulp/bundle-scss.js
+++ gulp/bundle-scss.js
@@ -2,13 +2,15 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     livereload = require('gulp-livereload');
+    normalize = require('node-normalize-scss'),
+    neat = require('node-neat');
 
 function bundleCss(src, dest) {
   return function() {
     gulp.src(src)
       .pipe(sourcemaps.init())
       .pipe(sass({
-        // includePaths: [].concat(normalize.includePaths, neat.includePaths),
+        includePaths: [].concat(normalize.includePaths, neat.includePaths),
         sourceMap: true,
         error: function(error) {
           console.log(error);
